a65e55b6b3e510d66cd6be8e871cdb0a9fd430e1
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-04-27 01:46:53 +0530

    Learned Flask

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///sunny.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.username}"
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world(): # put application's code here
+    sunny = User(username="Sunny", password="Sunnykd123")
+    db.session.add(sunny)
+    db.session.commit()
     return 'Hello World!'
 
 
